d8c4f658a7a5fad58f3872f251eaa7416c813796
yousufsadiq11 <yousufsadiq11@gmail.com>
2017-03-19 22:29:12 -0400

    Added Comments to CalculateIterativePageRankMapper

--- src/wikipedia/org/CalculateIterativePageRankMapper.java
+++ src/wikipedia/org/CalculateIterativePageRankMapper.java
@@ -1,59 +1,62 @@
 package wikipedia.org;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class CalculateIterativePageRankMapper extends
 		Mapper<LongWritable, Text, Text, Text> {
-
+	String tab_seperator="\t";
+	String addText="EliminatingRedLinks";
+	String outlinks_passing="!@#";
 	public void map(LongWritable offset, Text lineText, Context context)
 			throws IOException, InterruptedException {
-try{
-		context.getInputSplit();
-		int length = 0;
-		String line = lineText.toString();
-		String splitted[] = new String[3] ;
-		splitted[1]="";
-		splitted[0]="";
-		splitted[2]="";
-		System.out.println("asxaascac");
-		splitted = line.split("\t");System.out.println(splitted.length);
-		String page_title = splitted[0];
-		String page_rank_value = splitted[1];
-		String link_list = "";if (splitted.length==2){
-			context.write(new Text(page_title), new Text("EliminatingRedLinks"));context.write(new Text(page_title),new Text(page_rank_value + "\t" + 1));return;}
-		if (!(splitted[2].equals(""))) {
-			link_list = splitted[2];
-			System.out.println(page_title + " " + page_rank_value + " "
-					+ link_list);
-		}
-		context.write(new Text(page_title), new Text("EliminatingRedLinks"));
-		
-		String further_split_all_links[] = link_list.split(";;;");
-		System.out.println("furtherrrrrrrrrrr        "
-				+ further_split_all_links[0]);
-		length = further_split_all_links.length;
-		if (length>0) {
-			for (int i=0;i<further_split_all_links.length;i++) {
-				context.write(new Text(further_split_all_links[i]), new Text(page_rank_value + "\t"
-						+ length));
-				System.out.println(length + "loop" + further_split_all_links[i] + page_rank_value
-						+ length);
+		try {
+			context.getInputSplit();
+			int length = 0;
+			String line = lineText.toString();
+			String splitted[] = new String[3];
+			splitted[1] = "";
+			splitted[0] = "";
+			splitted[2] = "";
+			splitted = line.split(tab_seperator);
+			String page_title = splitted[0];
+			String page_rank_value = splitted[1];
+			String link_list = "";
+			// Checking if the corresponding has any outlinks
+			// Skipping the steps if there are no outlinks
+			if (splitted.length == 2) {
+				context.write(new Text(page_title), new Text(
+						addText));
+				context.write(new Text(page_title), new Text(page_rank_value
+						+ tab_seperator + 1));
+				return;
 			}
+			// Checking if there is empty string in outlinks
+			if (!(splitted[2].equals(""))) {
+				link_list = splitted[2];
+			}
+			// Seperating it's corresponding outlinks
+			context.write(new Text(page_title), new Text(addText));
+			String further_split_all_links[] = link_list.split(";;;");
+			length = further_split_all_links.length;
+			// Adding outlink followed by pagerank and count of outlinks as output
+			// length > 0 indicates there is atleast one outlinks
+			if (length > 0) {
+				for (int i = 0; i < further_split_all_links.length; i++) {
+					context.write(new Text(further_split_all_links[i]),
+							new Text(page_rank_value + tab_seperator + length));
+				}
+			} 
+			// Writing 1 as length if there are no outlinks
+			else
+				context.write(new Text(""),
+						new Text(page_rank_value + tab_seperator + 1));
+			context.write(new Text(page_title), new Text(outlinks_passing + link_list));
 		} 
-		else	
-			context.write(new Text(""), new Text(page_rank_value + "\t" + 1));
-		context.write(new Text(page_title), new Text("!@#"+link_list));
-	}catch(Exception e){
-		e.printStackTrace();
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	}
-	
 }
