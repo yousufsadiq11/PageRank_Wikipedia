68ca333a8aeb3499a1c7da91a571f678c6fe9a44
yousufsadiq11 <yousufsadiq11@gmail.com>
2017-03-19 22:07:10 -0400

    Updated Driver with Comments

--- src/wikipedia/org/Driver.java
+++ src/wikipedia/org/Driver.java
@@ -2,7 +2,6 @@ package wikipedia.org;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -18,33 +17,40 @@ import org.apache.hadoop.util.ToolRunner;
 public class Driver extends Configured implements Tool {
 
 	public int run(String[] args) throws Exception {
-
+		// Calling Jobs
+		boolean success = calculateNumberOfTitles(args);
+		success = calculateInitialPageRank(args);
+		success = computeIterations(10, args);
+		success = sortPageRank(args);
+		return success ? 0 : 1;
+	}
+	
+	// Job to calculate total Number of Pages
+	private boolean calculateNumberOfTitles(String[] args) throws Exception {
+		// TODO Auto-generated method stub
 		Job job1 = Job
 				.getInstance(getConf(), "Calculate Total Number Of Links");
 		job1.setJarByClass(this.getClass());
 		// Instantiating it's Mapper, and Reducer classes
 		job1.setMapperClass(CalculateTotalLinksMapper.class);
 		job1.setReducerClass(CalculateTotalLinksReducer.class);
-		// Instantiating input and output paths for Job 2
+		// Instantiating input and output paths for Job 1
 		FileInputFormat.addInputPath(job1, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job1, new Path(args[1] + "driver"));
+		FileOutputFormat.setOutputPath(job1, new Path(args[1] + "File_Count"));
 		// Instantiating corresponding output classes
 		job1.setMapOutputKeyClass(Text.class);
 		job1.setMapOutputValueClass(IntWritable.class);
 		job1.setOutputKeyClass(Text.class);
 		job1.setOutputValueClass(IntWritable.class);
-		boolean success = job1.waitForCompletion(true);
-		success = InitialPageRank(args);
-		success = Iteration(10, args);
-		success = sortingPageRank(args);
-		return success ? 0 : 1;
+		return job1.waitForCompletion(true);
 	}
-
-	private boolean sortingPageRank(String[] args) throws Exception {
+	
+	// Job to sort pageRank in descending order
+	private boolean sortPageRank(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		String input = args[1] + "Iteration_count_2";
+		// Setting it's input and output paths
+		String input = args[1] + "Iteration_count_10";
 		String output = args[1] + "Sorted_PageRank";
-		// DescendingKeyComparator obj=new DescendingKeyComparator();
 		Job job4 = Job.getInstance(getConf(), "Sorting Page Rank Values");
 		job4.setJarByClass(this.getClass());
 		// Instantiating Mapper and Reducer Classes
@@ -63,7 +69,8 @@ public class Driver extends Configured implements Tool {
 		return success;
 	}
 
-	private boolean Iteration(int n, String args[]) throws Exception {
+	// Method to compute 'n' Iterations
+	private boolean computeIterations(int n, String args[]) throws Exception {
 		// TODO Auto-generated method stub
 		boolean check = false;
 		for (int iterations = 0; iterations < n; iterations++) {
@@ -76,11 +83,11 @@ public class Driver extends Configured implements Tool {
 		return check;
 	}
 
+	// Job to compute Iterative Page Ranks
 	private boolean computeIterativePageRank(String input, String output)
 			throws Exception {
 		// TODO Auto-generated method stub
 		boolean success = false;
-		System.out.println(input + " " + output);
 		Job job3 = Job.getInstance(getConf(), "Calculate Iterative Page Rank");
 		job3.setJarByClass(Driver.class);
 		// Instantiating it's Mapper, and Reducer classes
@@ -95,17 +102,16 @@ public class Driver extends Configured implements Tool {
 		job3.setOutputKeyClass(Text.class);
 		job3.setOutputValueClass(Text.class);
 		success = job3.waitForCompletion(true);
-
-		// }
 		return success;
 	}
 
-	public boolean InitialPageRank(String[] args) throws Exception {
+	// Job to calculate Initial Page Rank
+	public boolean calculateInitialPageRank(String[] args) throws Exception {
+		// Configuration object containing Size of the corpus from Job 1
 		Configuration conf = new Configuration();
 		double title_count = 0;
-
 		BufferedReader in = new BufferedReader(new FileReader(args[1]
-				+ "driver/part-r-00000"));
+				+ "File_Count/part-r-00000"));
 		String str;
 		while ((str = in.readLine()) != null) {
 			String splitted[] = str.split("\t");
@@ -132,6 +138,7 @@ public class Driver extends Configured implements Tool {
 		return success;
 	}
 
+	// Invokes run method to run Jobs
 	public static void main(String[] args) throws Exception {
 		int res = ToolRunner.run(new Driver(), args);
 		System.exit(res);
