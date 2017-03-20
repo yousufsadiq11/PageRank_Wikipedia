1c8ef24eeda96acc878cdc898325ca36c7978b7f
yousufsadiq11 <yousufsadiq11@gmail.com>
2017-03-19 20:42:00 -0400

    Modified Mapper

--- src/wikipedia/org/FinalSortedRankMapper.java
+++ src/wikipedia/org/FinalSortedRankMapper.java
@@ -1,23 +1,26 @@
 package wikipedia.org;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class FinalSortedRankMapper extends
-		Mapper<LongWritable, Text, Text, DoubleWritable> {
-	String tab_seperator="\\t";
+		Mapper<LongWritable, Text, DoubleWritable, Text> {
+	String tab_seperator = "\\t";
+
 	public void map(LongWritable offset, Text lineText, Context context)
 			throws IOException, InterruptedException {
 		context.getInputSplit();
-		String line = lineText.toString();
-		String splitted[]=line.split(tab_seperator);
-		context.write(new Text(splitted[0]),new DoubleWritable(Double.parseDouble(splitted[1])));
+		try {
+			String line = lineText.toString();
+			String splitted[] = line.split(tab_seperator);
+			context.write(new DoubleWritable(Double.parseDouble(splitted[1])),
+					new Text(splitted[0]));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
