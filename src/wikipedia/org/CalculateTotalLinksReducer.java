dca4481eb93c6ac8e39d167f5faf99cb6c20a58e
yousufsadiq11 <yousufsadiq11@gmail.com>
2017-03-19 22:39:16 -0400

    Added Comments to CalculateTotalLinksReducer

--- src/wikipedia/org/CalculateTotalLinksReducer.java
+++ src/wikipedia/org/CalculateTotalLinksReducer.java
@@ -2,14 +2,10 @@ package wikipedia.org;
 
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.ArrayWritable;
-import org.apache.hadoop.io.DoubleWritable;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 
 public class CalculateTotalLinksReducer extends
 		Reducer<Text, IntWritable, Text, IntWritable> {
@@ -17,6 +13,7 @@ public class CalculateTotalLinksReducer extends
 	public void reduce(Text word, Iterable<IntWritable> values,
 			Context context) throws IOException, InterruptedException {
 		int sum=0;
+		// Computing the count of pages extracted from XML data
 		for(IntWritable count:values){
 			sum=sum+count.get();
 		}
