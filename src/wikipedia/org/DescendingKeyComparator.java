e09618a4ff60cadc9d1ff98181b098e50cd7aa9b
yousufsadiq11 <yousufsadiq11@gmail.com>
2017-03-19 20:39:19 -0400

    Comparator added to sort according to values

---
+++ src/wikipedia/org/DescendingKeyComparator.java
@@ -0,0 +1,19 @@
+package wikipedia.org;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class DescendingKeyComparator extends WritableComparator {
+	protected DescendingKeyComparator() {
+		super(DoubleWritable.class, true);
+	}
+
+	@Override
+	public int compare(WritableComparable w1, WritableComparable w2) {
+		DoubleWritable d1 = (DoubleWritable) w1;
+		DoubleWritable d2 = (DoubleWritable) w2;
+		int cmp = d1.compareTo(d2);
+		return cmp * -1; // reverse
+	}
+}
